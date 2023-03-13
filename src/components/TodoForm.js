c3a23150320117f6e521af24f171e1cdf873ac0a
Ana Sofia <sofiacodifica@gmail.com>
2023-03-13 13:10:02 -0500

    [AS] Versión 1

---
+++ src/components/TodoForm.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+
+export const TodoForm = ({addTodo}) => {
+  const [value, setValue] = useState("")
+  const handleSubmit = e => {
+    e.preventDefault();
+    addTodo(value);
+    setValue("")
+  }
+  return (
+    <form className='TodoForm' onSubmit={handleSubmit}>
+      <input type="text" className='todo-input' value = {value} placeholder='Ingrese una tarea' onChange={(e) => setValue(e.target.value)}/>
+      <button type='submit' className='todo-btn'>Agregar</button>
+    </form>
+  )
+}
